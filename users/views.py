8a6268aba3b8b79a88e9af174c2fa609b4e8c19b
mariposa <salikova.work@gmail.com>
2024-01-21 12:41:01 +0300

    registration realized, managed the staff and usual users fields from navbar

--- users/views.py
+++ users/views.py
@@ -2,9 +2,9 @@
 # Create your views here.
 from django.contrib import auth
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
-from users.forms import UserLoginForm
+from users.forms import UserLoginForm, UserRegistrationForm
 
 def login(request):
     if request.method == "POST":
@@ -28,8 +28,19 @@ def login(request):
 
 
 def registration(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            return HttpResponseRedirect(reverse('main:index'))
+    else:
+        #si no había enviado methodo POST ya - если еще не отправился пост-метод
+        form = UserRegistrationForm()
     context = {
-        'title':'Home - re'
+        'title':'Home - registration',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
@@ -42,4 +53,5 @@ def profile(request):
 
 
 def logout(request):
-    ...
+    auth.logout(request)
+    return redirect(reverse('main:index'))
